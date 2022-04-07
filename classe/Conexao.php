a829eb24981e070aac239f5ea21ea50cd738f6ff
Nilton-3923 <64760713+Nilton-3923@users.noreply.github.com>
2022-04-07 20:43:30 -0300

    Criando a classe Conexao

---
+++ classe/Conexao.php
@@ -0,0 +1,28 @@
+<!-- Classe para poder concetar com o banco de dados --> 
+<?php
+class Conexao{
+	public static function pegarConexao(){
+
+		//Variaveis para facilitar a conexão com o banco
+		$servidor="localhost";
+		$banco="bdcidadelimpa";
+		$usuario="root";
+		$senha="";
+	
+		//Método PDO para conectar com o banco de dados 
+		$conexao = new PDO("mysql:host=$servidor;
+						dbname=$banco",
+						$usuario,
+						$senha);
+
+
+		//Caso dê erro,retornar o erro
+		$conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+		//Está executando um código de SQL no banco par poder usar caracteres especiais 
+		$conexao->exec("SET CHARACTER SET utf8");
+
+		return $conexao;
+	}
+}
+?>
