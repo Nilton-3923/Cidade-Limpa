de7974b01461d5bf26bb6a9aa0f36d5bdaa8f1d3
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-04-12 15:01:30 -0300

    Add files via upload

--- bdcidadelimpa.sql
+++ bdcidadelimpa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Abr-2022 às 20:23
+-- Tempo de geração: 12-Abr-2022 às 19:58
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.13
 
@@ -24,12 +24,65 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tbcategoria`
+--
+
+CREATE TABLE `tbcategoria` (
+  `pk_idCategoria` int(11) NOT NULL,
+  `campoCategoria` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbdenuncia`
+--
+
+CREATE TABLE `tbdenuncia` (
+  `pk_idDenuncia` int(11) NOT NULL,
+  `descDenuncia` varchar(255) NOT NULL,
+  `tituloDenuncia` varchar(30) NOT NULL,
+  `dataDenuncia` date NOT NULL,
+  `ufDenuncia` varchar(2) NOT NULL,
+  `logradouroDenuncia` varchar(30) DEFAULT NULL,
+  `bairroDenuncia` varchar(50) NOT NULL,
+  `cepDenuncia` varchar(8) DEFAULT NULL,
+  `fk_idImgDenun` int(11) DEFAULT NULL,
+  `fk_idUsuario` int(11) DEFAULT NULL,
+  `fk_idCategoria` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbimgdenun`
+--
+
+CREATE TABLE `tbimgdenun` (
+  `pk_idImgDenun` int(11) NOT NULL,
+  `imgDenuncia` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbimgusuario`
+--
+
+CREATE TABLE `tbimgusuario` (
+  `pk_idImgUsuario` int(11) NOT NULL,
+  `imgUsuario` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tbtelusuario`
 --
 
 CREATE TABLE `tbtelusuario` (
-  `pk_idTelUsuario` int(11) NOT NULL,
-  `numTelUsuario` varchar(14) NOT NULL,
+  `pk_TelUsuario` int(11) NOT NULL,
+  `numTelUsuario` varchar(11) NOT NULL,
   `fk_idUsuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,11 +93,12 @@ CREATE TABLE `tbtelusuario` (
 --
 
 CREATE TABLE `tbusuario` (
-  `pk_idUsuario` int(11) NOT NULL,
-  `nomeUsuario` varchar(100) NOT NULL,
-  `sobrenomeUsuario` varchar(100) NOT NULL,
+  `pk_Usuario` int(11) NOT NULL,
+  `nomeUsuario` varchar(150) NOT NULL,
   `emailUsuario` varchar(150) NOT NULL,
-  `cepUsuario` varchar(9) NOT NULL
+  `senhaUsuario` varchar(150) NOT NULL,
+  `cepUsuario` varchar(8) NOT NULL,
+  `fk_idImgUsuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -52,33 +106,109 @@ CREATE TABLE `tbusuario` (
 --
 
 --
+-- Índices para tabela `tbcategoria`
+--
+ALTER TABLE `tbcategoria`
+  ADD PRIMARY KEY (`pk_idCategoria`);
+
+--
+-- Índices para tabela `tbdenuncia`
+--
+ALTER TABLE `tbdenuncia`
+  ADD PRIMARY KEY (`pk_idDenuncia`),
+  ADD UNIQUE KEY `fk_idImgDenun` (`fk_idImgDenun`),
+  ADD UNIQUE KEY `fk_idUsuario` (`fk_idUsuario`),
+  ADD UNIQUE KEY `fk_idCategoria` (`fk_idCategoria`);
+
+--
+-- Índices para tabela `tbimgdenun`
+--
+ALTER TABLE `tbimgdenun`
+  ADD PRIMARY KEY (`pk_idImgDenun`);
+
+--
+-- Índices para tabela `tbimgusuario`
+--
+ALTER TABLE `tbimgusuario`
+  ADD PRIMARY KEY (`pk_idImgUsuario`);
+
+--
 -- Índices para tabela `tbtelusuario`
 --
 ALTER TABLE `tbtelusuario`
-  ADD PRIMARY KEY (`pk_idTelUsuario`),
-  ADD UNIQUE KEY `fk_idUsuario` (`fk_idUsuario`);
+  ADD PRIMARY KEY (`pk_TelUsuario`),
+  ADD KEY `fk_idUsuario` (`fk_idUsuario`);
 
 --
 -- Índices para tabela `tbusuario`
 --
 ALTER TABLE `tbusuario`
-  ADD PRIMARY KEY (`pk_idUsuario`);
+  ADD PRIMARY KEY (`pk_Usuario`),
+  ADD UNIQUE KEY `fk_idImgUsuario` (`fk_idImgUsuario`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
+-- AUTO_INCREMENT de tabela `tbcategoria`
+--
+ALTER TABLE `tbcategoria`
+  MODIFY `pk_idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbdenuncia`
+--
+ALTER TABLE `tbdenuncia`
+  MODIFY `pk_idDenuncia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbimgdenun`
+--
+ALTER TABLE `tbimgdenun`
+  MODIFY `pk_idImgDenun` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbimgusuario`
+--
+ALTER TABLE `tbimgusuario`
+  MODIFY `pk_idImgUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `tbtelusuario`
 --
 ALTER TABLE `tbtelusuario`
-  MODIFY `pk_idTelUsuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pk_TelUsuario` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `tbusuario`
 --
 ALTER TABLE `tbusuario`
-  MODIFY `pk_idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pk_Usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `tbdenuncia`
+--
+ALTER TABLE `tbdenuncia`
+  ADD CONSTRAINT `tbdenuncia_ibfk_1` FOREIGN KEY (`fk_idCategoria`) REFERENCES `tbcategoria` (`pk_idCategoria`),
+  ADD CONSTRAINT `tbdenuncia_ibfk_2` FOREIGN KEY (`fk_idImgDenun`) REFERENCES `tbimgdenun` (`pk_idImgDenun`),
+  ADD CONSTRAINT `tbdenuncia_ibfk_3` FOREIGN KEY (`fk_idUsuario`) REFERENCES `tbusuario` (`pk_Usuario`);
+
+--
+-- Limitadores para a tabela `tbtelusuario`
+--
+ALTER TABLE `tbtelusuario`
+  ADD CONSTRAINT `tbtelusuario_ibfk_1` FOREIGN KEY (`fk_idUsuario`) REFERENCES `tbusuario` (`pk_Usuario`);
+
+--
+-- Limitadores para a tabela `tbusuario`
+--
+ALTER TABLE `tbusuario`
+  ADD CONSTRAINT `tbusuario_ibfk_1` FOREIGN KEY (`fk_idImgUsuario`) REFERENCES `tbimgusuario` (`pk_idImgUsuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
