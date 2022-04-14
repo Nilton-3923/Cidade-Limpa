a5e5798cedba5c05d71d14318d7113b1744f7982
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-04-14 00:38:45 -0300

    Add files via upload

---
+++ cadastro/objeto-cadastro-denuncia.php
@@ -0,0 +1,23 @@
+<?php 
+    require_once("../classe/Conexao.php");
+    require_once("../classe/Denuncia.php");
+
+    //Criação de um Objeto Denuncia
+
+    $denuncia = new Denuncia();
+
+    $denuncia->setTituloDenuncia($_POST['txtTituloDenuncia']);
+    $denuncia->setDescDenuncia($_POST['txtDenuncia']);
+
+    $denuncia->setCepDenuncia($_POST['txtCepDenuncia']);
+    $denuncia->setBairroDenuncia($_POST['txtBairroDenuncia']);
+    $denuncia->setRuaDenuncia($_POST['txtRuaDenuncia']);
+    $denuncia->setCidadeDenuncia($_POST['txtCidadeDenuncia']);
+    $denuncia->setDataDenuncia($_POST['txtDtDenuncia']);
+    $denuncia->setUfDenuncia($_POST['txtUfDenuncia']);
+    $denuncia->setLogradouroDenuncia($_POST['txtLogradouroDenuncia']);
+    
+
+    echo $denuncia->denunciar($denuncia);
+
+?>
