c3a8d2b03341a022ac09b27bdd56a3779aa290ce
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-04-19 13:11:09 -0300

    Delete session directory

--- session/logout-usuario.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['idUsuario']);
-    session_destroy();
-    header("Location: ../form-cadastro/login-usuario.php");
-?>
--- session/valida-sentinela-adm.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    if($_SESSION['emailAdm'] != 'adm' || $_SESSION['senhaAdm'] != '123'){
-        header("Location: ../form-cadastro/login-usuario.php");
-        exit();
-    }
-?>
