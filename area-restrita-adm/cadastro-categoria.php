4d4caa56f59a88859e32bdf2618d8819b00a8931
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-05-03 19:35:14 -0300

    Add files via upload

---
+++ area-restrita-adm/cadastro-categoria.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cadastro-Categoria</title>
+    </head>
+    <body>
+        <form action="../cadastro/objeto-cadastro-categoria.php" method="post"> 
+            <input type="text" name="txtCategoria" placeholder="Digite o nome da Categoria...">
+            <input type="submit" value="cadastrar">
+        </form>
+        
+    </body>
+</html>
