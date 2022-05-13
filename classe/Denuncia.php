e11874bea66fee12019842d8a628daff846ecd45
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-05-13 15:49:39 -0300

    Add files via upload

---
+++ classe/Denuncia.php
@@ -0,0 +1,276 @@
+<?php
+    require_once("Conexao.php");
+    require_once("Categoria.php");
+
+    class Denuncia{
+
+        private $idDenuncia;
+
+        private $descDenuncia;
+        private $tituloDenuncia;
+        private $dataDenuncia;
+
+        private $cepDenuncia;
+        private $ufDenuncia;
+        private $bairroDenuncia;
+        private $ruaDenuncia;
+        private $cidadeDenuncia;
+        private $numero; 
+
+        private $imagemDenuncia;
+        private $idUsuario;
+        private $idCategoria;
+
+
+ 
+        //Métodos Getters
+
+        # PK e FK
+        public function getIdDenuncia(){
+            return $this->idDenuncia;
+        }
+        public function getIdUsuario(){
+            return $this->idUsuario;
+        }
+        public function getIdCategoria(){
+            return $this->idCategoria;
+        }
+        
+
+        # Descrições,data e foto
+        public function getDescDenuncia(){
+            return $this->descDenuncia;
+        }
+        public function getTituloDenuncia(){
+            return $this->tituloDenuncia;
+        }
+        public function getDataDenuncia(){
+            return $this->dataDenuncia;
+        }
+        public function getImagemDenuncia(){
+            return $this->imagemDenuncia;
+        }
+
+        # Localização
+        public function getCepDenuncia(){
+            return $this->cepDenuncia;
+        }
+        public function getUfDenuncia(){
+            return $this->ufDenuncia;
+        }
+        public function getBairroDenuncia(){
+            return $this->bairroDenuncia;
+        }
+        public function getRuaDenuncia(){
+            return $this->ruaDenuncia;
+        }
+        public function getCidadeDenuncia(){
+            return $this->cidadeDenuncia;
+        }
+        public function getNumero(){
+            return $this -> numero; 
+        }
+
+
+        //Métodos Setters
+
+        #PK e FK
+        public function setIdDenuncia($idDenuncia){
+            $this->idDenuncia = $idDenuncia;
+        }
+        public function setIdUsuario($idUsuario){
+            $this->idUsuario = $idUsuario;
+        }
+        public function setIdCategoria($idCategoria){
+            $this->idCategoria = $idCategoria;
+        }
+
+
+
+        #Descrição,data e foto
+        public function setDescDenuncia($descDenuncia){
+            $this->descDenuncia = $descDenuncia;
+        }
+        public function setTituloDenuncia($tituloDenuncia){
+            $this->tituloDenuncia = $tituloDenuncia;
+        }
+        public function setDataDenuncia($dataDenuncia){
+            $this->dataDenuncia = $dataDenuncia;
+        }
+        public function setImagemDenuncia($imagemDenuncia){
+            $this->imagemDenuncia = $imagemDenuncia;
+        }
+
+        #Localização
+        public function setUfDenuncia($ufDenuncia){
+            $this->ufDenuncia = $ufDenuncia;
+        }
+        public function setBairroDenuncia($bairroDenuncia){
+            $this->bairroDenuncia = $bairroDenuncia;
+        }
+        public function setCepDenuncia($cepDenuncia){
+            $this->cepDenuncia = $cepDenuncia;
+        }
+        public function setRuaDenuncia($ruaDenuncia){
+            $this->ruaDenuncia= $ruaDenuncia;
+        }
+        public function setCidadeDenuncia($cidadeDenuncia){
+            $this->cidadeDenuncia = $cidadeDenuncia;
+        }
+        public function setNumero($numero){
+            $this -> numero = $numero; 
+        }
+
+
+        public function geolocalizacao(){
+
+            $ufDenuncia = $this -> ufDenuncia; 
+            $cidadeDenuncia = $this -> cidadeDenuncia;
+            $ruaDenuncia = $this -> ruaDenuncia;
+            $bairroDenuncia = $this -> bairroDenuncia;
+            $numero = $this -> numero; 
+
+
+            $enderecoJunto = "$numero $ruaDenuncia, $bairroDenuncia, $cidadeDenuncia - $ufDenuncia"; 
+
+            $addr = str_replace(" ", "+","$enderecoJunto");
+
+            $address = utf8_encode($addr);
+    
+            $url = "https://maps.googleapis.com/maps/api/geocode/json?address=$address&key=AIzaSyAo1uIjwfM3QBcwPKKDVuXv0z8eJlQGcYE";
+        
+            $json = file_get_contents($url);
+            $data = json_decode($json);
+    
+            $lat = $data->results[0]->geometry->location->lat;
+            $long = $data->results[0]->geometry->location->lng;
+
+            $localizacao = "lat: $lat, lng: $long"; 
+    
+            return $localizacao; 
+        }
+
+
+
+
+        public function denunciar($denuncia){
+            $conexao = Conexao::pegarConexao();
+
+            //Cadastro da Denúncia
+            $stmtDenuncia = $conexao->prepare(
+                "INSERT INTO tbDenuncia
+                    (tituloDenuncia,#1
+                    descDenuncia,   #2
+                    imgDenuncia,    #3
+                    dataDenuncia,   #4
+                    ufDenuncia,     #5
+                    bairroDenuncia, #6
+                    cepDenuncia,    #7
+                    ruaDenuncia,    #8
+                    cidadeDenuncia, #9
+                    coordeDenuncia, #10
+                    fk_idUsuario,   #11                   
+                    fk_idCategoria) #12
+                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"
+            );
+
+            //Informações
+            $stmtDenuncia->bindValue(1,$denuncia->getTituloDenuncia());
+            $stmtDenuncia->bindValue(2,$denuncia->getDescDenuncia());
+            $stmtDenuncia->bindValue(3,$denuncia->getImagemDenuncia());
+            $stmtDenuncia->bindValue(4,$denuncia->getDataDenuncia());
+
+            //Endereços
+            $stmtDenuncia->bindValue(5,$denuncia->getUfDenuncia());
+            $stmtDenuncia->bindValue(6,$denuncia->getBairroDenuncia());
+            $stmtDenuncia->bindValue(7,$denuncia->getCepDenuncia());
+            $stmtDenuncia->bindValue(8,$denuncia->getRuaDenuncia());
+            $stmtDenuncia->bindValue(9,$denuncia->getCidadeDenuncia());
+
+            //Geolocalização
+            $stmtDenuncia-> bindValue(10,$denuncia->geolocalizacao());
+
+            //Id
+            $stmtDenuncia->bindValue(11,$denuncia->getIdUsuario());
+            $stmtDenuncia->bindValue(12,$denuncia->getIdCategoria());
+
+            $stmtDenuncia->execute();
+        }
+        
+
+        //Método para Mostrar as Denúncias já feitas
+        public function mostrar(){
+            $conexao = Conexao::pegarConexao();
+
+            $query = "SELECT imgDenuncia,imgUsuario,nomeUsuario,tituloDenuncia,descDenuncia,dataDenuncia,ufDenuncia,bairroDenuncia,cepDenuncia,ruaDenuncia,cidadeDenuncia FROM tbDenuncia
+                      INNER JOIN tbUsuario ON tbDenuncia.fk_idUsuario = tbUsuario.pk_Usuario";
+
+            $query = $conexao->query($query);
+
+            $query = $query->fetchAll();
+
+            return $query;
+
+
+
+        }
+
+        public function mostrarPontosMapa(){
+            $conexao = Conexao :: pegarConexao();
+
+            $query = "SELECT coordeDenuncia, tituloDenuncia, descDenuncia, cepDenuncia, DATE_FORMAT(`dataDenuncia`,'%d/%m/%Y') as dataDenuncia, campoCategoria,imgDenuncia FROM tbDenuncia 
+                INNER JOIN tbcategoria 
+                    ON tbcategoria.pk_idCategoria = tbdenuncia.fk_idCategoria       
+            ";
+
+            $query = $conexao->query($query);
+            $query = $query->fetchAll();
+
+            return $query;
+        }
+
+        //Método de Deletar Denuncia(DELETE)
+        public function deletarDenuncia($idDenuncia,$imagemDenuncia){
+            $conexao = Conexao::pegarConexao();
+
+            $deleteDenuncia = $conexao->prepare("DELETE FROM tbDenuncia
+                                                WHERE pk_idDenuncia = $idDenuncia");
+
+            $deleteDenuncia->execute();
+            return "Deletado";
+
+        }
+        
+        //Método de Alterar Denuncia 
+        public function alterar($idDenuncia, $titulo, $desc, $caminhoImagem){
+            $conexao = Conexao::pegarConexao();
+
+            $alterarDenuncia = $conexao->prepare("UPDATE tbDenuncia 
+                                                    SET
+                                                    tituloDenuncia = '$titulo'
+                                                    ,descDenuncia = '$desc'
+                                                    ,imgDenuncia = '$caminhoImagem'
+                                                        WHERE pk_idDenuncia = '$idDenuncia'");
+
+            $alterarDenuncia->execute();
+
+
+            return "Update realizado";      
+        }
+
+        //Método de pesquisa
+        public function pesquisar($pesquisar){
+            $conexao = Conexao::pegarConexao();
+
+            $query = "SELECT tituloDenuncia,descDenuncia,dataDenuncia,nomeUsuario,imgUsuario,cepDenuncia,imgDenuncia FROM tbDenuncia
+                      INNER JOIN tbUsuario ON tbUsuario.pk_Usuario = tbDenuncia.fk_idUsuario
+                      WHERE tituloDenuncia LIKE '%$pesquisar%' OR descDenuncia LIKE '%$pesquisar%' OR nomeUsuario LIKE '%$pesquisar%'";
+
+            $query = $conexao->query($query);
+            $query = $query->fetchAll();
+            return $query;
+        }
+
+
+    }
+?>
