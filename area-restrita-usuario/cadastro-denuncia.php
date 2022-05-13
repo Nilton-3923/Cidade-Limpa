dedb33f8a8b45d95abc0ce61c7c8a0c784673f22
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-05-13 15:51:26 -0300

    Add files via upload

---
+++ area-restrita-usuario/cadastro-denuncia.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--NILTON FEZ ALTERAÇÃO-->
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Denuncia</title>
+        
+    </head>
+    <body>
+
+    
+  
+    </body>
+    
+</html>
