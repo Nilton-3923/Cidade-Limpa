58f6126b35a007394c9b4d5a6a6814e6aade8ec6
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-05-17 00:05:06 -0300

    Add files via upload

---
+++ cadastro/objeto-cadastro-categoria.php
@@ -0,0 +1,13 @@
+<?php
+    include_once("../classe/Conexao.php");
+    include_once("../classe/Categoria.php");
+
+
+    $categoria = new Categoria();
+
+    $categoria->setCampoCategoria($_POST['txtCategoria']);
+    echo $categoria->cadastrar($categoria);
+
+    header("Location: ../area-restrita-adm/index-adm-restrita.php");
+
+?> 
---
+++ cadastro/objeto-cadastro-denuncia.php
@@ -0,0 +1,39 @@
+<?php 
+    require_once("../classe/Conexao.php");
+    require_once("../classe/Denuncia.php");
+    require_once("../classe/Categoria.php");
+
+    //Criação de um Objeto Denuncia
+    //Cadastro da Foto da Denuncia
+    $nomeImagem = $_FILES['fotoDenuncia']['name'];
+    $arquivoImagem = $_FILES['fotoDenuncia']['tmp_name'];
+
+    $caminhoImagem = "imgDenuncia/".$nomeImagem;
+    move_uploaded_file($arquivoImagem,$caminhoImagem);
+
+    $denuncia = new Denuncia();
+
+    $denuncia->setTituloDenuncia($_POST['txtTituloDenuncia']); 
+    $denuncia->setDescDenuncia($_POST['txtDenuncia']);
+    $denuncia->setImagemDenuncia($caminhoImagem);
+
+    $denuncia->setCepDenuncia($_POST['txtCepDenuncia']);
+    $denuncia->setBairroDenuncia($_POST['txtBairroDenuncia']);
+    $denuncia->setRuaDenuncia($_POST['txtRuaDenuncia']);
+    $denuncia->setCidadeDenuncia($_POST['txtCidadeDenuncia']);
+    $denuncia->setUfDenuncia($_POST['txtUfDenuncia']);
+    $denuncia-> setNumero($_POST['txtNumeroDenuncia']);
+    
+    
+    $denuncia->setDataDenuncia($_POST['txtDtDenuncia']);
+    $denuncia->setIdUsuario($_POST['txtIdUsuario']);
+    
+    $denuncia->setIdCategoria($_POST['txtCategoria']);
+
+
+    echo $denuncia->denunciar($denuncia);
+    
+    echo $denuncia->corpoEmail();
+    //header("Location:../area-restrita-usuario/index-restrita.php"); 
+
+?>
