8e3c4191293d93b531fa37b3e1b73eee8d3de79f
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-05-31 22:25:09 -0300

    Update index-adm-restrita.php

--- area-restrita-adm/index-adm-restrita.php
+++ area-restrita-adm/index-adm-restrita.php
@@ -9,7 +9,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-	<link rel="stylesheet" href="../css/adm.css">
+	<link rel="stylesheet" href="../css/index-adm.css">
 	<title>AdminSite</title>
 </head>
 	<body>
