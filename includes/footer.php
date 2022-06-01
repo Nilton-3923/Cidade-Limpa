be3303973e2283572ecce3c148c16ce697ccd605
Lucas Nobre <arthgamer13@gmail.com>
2022-06-01 12:09:54 -0300

    sei la que porra acnteceu

--- includes/footer.php
+++ includes/footer.php
@@ -9,16 +9,20 @@
 </head>
 <body>
     <div class="footer">
-        <div class="softone">
-            <p class="s">S</p>
-            <div class="div-1-hex">
-                <div class="hexagono"></div>
+            <div class="div-1">
+                <img class="icone-footer"src="./imagens/LogoSoftOne.png">                
+                <img class="icone-footer"src="./imagens/linkedin.png">               
+                <img class="icone-footer"src="./imagens/facebook.png">
             </div>
-            <div class="div-2-hex">
-                <div class="hexagono"></div>
-                <div class="hexagono"></div>
+            <div class="div-2">
+                <a class="link" href="#">Home</a>
+                <a class="link" href="#">Termos</a>
+                <a href="https://www.iubenda.com/privacy-policy/15474164" class="iubenda-black iubenda-noiframe iubenda-embed iubenda-noiframe " title="Política de Privacidade ">Política de Privacidade</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
             </div>
-        </div>
+            <div class="div-3">
+                <p>SoftOne © 2022 - Lucas nobre | Nilton Gabriel | Nycolas Freitas | Guilherme de Lima |. . . </p>
+            </div>
+
     </div>
 </body>
 </html>
